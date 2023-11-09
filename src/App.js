77f313cb75c55808ae3681a685399269f9d8ea61
Rafael Christian <rafaelchristian003@gmail.com>
2023-11-09 22:35:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './Home';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+  const authorizedUsers = [
+    { username: 'raf', password: '123' },
+    { username: 'user2', password: 'password2' },
+  ];
+
+  const handleLogin = () => {
+    const user = authorizedUsers.find(user => user.username === username && user.password === password);
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoginError(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {loggedIn ? (
+        < div className="container-fluid">
+          <Home />
+
+        </div>
+      ) : (
+        <div className="Container displayFlex">
+          <div className="login-container">
+            <h2 className="login-heading">Login</h2>
+            {loginError && (
+              <div className="error-message">Login failed. Please check your credentials.</div>
+            )}
+            <form className="login-form">
+              <div className={`form-group ${loginError ? 'error' : ''}`}>
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className="form-control"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className={`form-group ${loginError ? 'error' : ''}`}>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <button type="button" className="login-button" onClick={handleLogin}>Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
